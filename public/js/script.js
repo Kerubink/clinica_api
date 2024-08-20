f03af0a874c2fb180186f0ed0ad36ec8c29b091c
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-08-20 16:22:24 -0300

    Feat: configuração da biblioteca  reveal

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.title');
+ScrollReveal().reveal('.text-principal');
+ScrollReveal().reveal('.btn');
+ScrollReveal().reveal('.img-principal');
+ScrollReveal().reveal('.title-about');
+ScrollReveal().reveal('.img-about', {delay: 500});
+ScrollReveal().reveal('.text-about', {delay: 1000});
