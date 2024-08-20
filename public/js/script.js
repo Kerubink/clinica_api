d43a4811f4a988e476ec8f8cc1ca54b546d40651
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-08-20 17:21:19 -0300

    Fix: modificando estrutura html

--- public/js/script.js
+++ public/js/script.js
@@ -3,5 +3,6 @@ ScrollReveal().reveal('.text-principal');
 ScrollReveal().reveal('.btn');
 ScrollReveal().reveal('.img-principal');
 ScrollReveal().reveal('.title-about');
+ScrollReveal().reveal('.title-sections', {delay: 250});
 ScrollReveal().reveal('.img-about', {delay: 500});
 ScrollReveal().reveal('.text-about', {delay: 1000});
